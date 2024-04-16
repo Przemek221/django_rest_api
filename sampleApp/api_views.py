50ba1a0a4386d603b8b03e821cafd195039b9d5d
Przemek221 <przemyslawpolej@wp.pl>
2024-04-16 23:28:40 +0200

    Different serializer based on REST action type

--- sampleApp/api_views.py
+++ sampleApp/api_views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-from .serializers import PostSerializer, UserSerializer, CommentSerializer
+from .serializers import PostSerializer, UserSerializer, CommentSerializer, PostDetailsSerializer
 from .models import Post, Comment
 from rest_framework.pagination import PageNumberPagination
 
@@ -18,11 +18,16 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     pagination_class = PostPaginationClass
 
+    def get_serializer_class(self):
+        if hasattr(self, 'action') and self.action == 'retrieve':
+            return PostDetailsSerializer
+        return PostSerializer  # I don't know what you want for create/destroy/update.
+
     # retrieve is used when returning single item
-    def retrieve(self, request, *args, **kwargs):
-        queryset = super().retrieve(request, *args, **kwargs)
-        queryset.data['test_data'] = 0
-        return queryset
+    # def retrieve(self, request, *args, **kwargs):
+    #     queryset = super().retrieve(request, *args, **kwargs)
+    #     queryset.data['test_data'] = 0
+    #     return queryset
 
     # list is used when returning multiple objects
     # def list(self, request, *args, **kwargs):
@@ -38,4 +43,3 @@ class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     # https://stackoverflow.com/questions/67962024/how-to-query-related-object-in-drf-viewsets-modelviewset
     serializer_class = CommentSerializer
-
--- sampleApp/serializers.py
+++ sampleApp/serializers.py
@@ -2,23 +2,43 @@ from rest_framework import serializers
 from .models import *
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email']
-
-
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
 
 
+class PostAttachmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostAttachment
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    # probably not needed to see comments here
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'comments']
+
+
 class PostSerializer(serializers.ModelSerializer):
+    attachments = PostAttachmentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        # model = PostAttachment
+        fields = '__all__'
+        # with depth>=0 the return json contains creator details, with depth==0 there is only creator's id
+        # depth = 1
+
+
+class PostDetailsSerializer(serializers.ModelSerializer):
     # Allows to find comments by 'related name' from models.py
     # https://stackoverflow.com/questions/46260695/django-rest-framework-get-related-objects
     comments = CommentSerializer(many=True, read_only=True)
-
+    attachments = PostAttachmentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Post
