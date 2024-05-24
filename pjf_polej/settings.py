753ffbb35c88f866d068ad8a9bd93b86a02b89eb
Przemek221 <przemyslawpolej@wp.pl>
2024-05-25 01:16:20 +0200

    changed simple_jwt token lifetime while debugging

--- pjf_polej/settings.py
+++ pjf_polej/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os.path
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -146,3 +147,9 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+SIMPLE_JWT = {
+}
+
+if DEBUG:
+    SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'] = timedelta(days=100)
